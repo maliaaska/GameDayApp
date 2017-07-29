01f3253b4588812e2a2af12024fe2c329a07a0ad
Arkadiusz <asmilewski@gmail.com>
2017-07-29 20:48:24 +0200

    correct routing through navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyLogInComponent } from './my-log-in/my-log-in.component';
+import { MySignUpComponent } from './my-sign-up/my-sign-up.component';
+import { MyAboutComponent }  from './my-about/my-about.component';
+
+
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home',  component: MyHomeComponent },
+  { path: 'logIn', component: MyLogInComponent },
+  { path: 'signUp', component: MySignUpComponent },
+  { path: 'about', component: MyAboutComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyLogInComponent,
+    MySignUpComponent,
+    MyAboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     HttpModule
   ],
   providers: [],
---
+++ src/app/my-log-in/my-log-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-log-in',
+  templateUrl: './my-log-in.component.html',
+  styleUrls: ['./my-log-in.component.css']
+})
+export class MyLogInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
