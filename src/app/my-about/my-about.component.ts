cc53975f8f92f04f2ee6295b52bda332df8f7987
Arkadiusz <asmilewski@gmail.com>
2017-07-31 13:43:59 +0200

    nav buttons routing works without refreshing page, click event to rout without refreshing page still to fix

--- src/app/my-about/my-about.component.ts
+++ src/app/my-about/my-about.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { MyHomeComponent } from "app/my-home/my-home.component";
 @Component({
   selector: 'app-my-about',
   templateUrl: './my-about.component.html',
   styleUrls: ['./my-about.component.css']
 })
+
+
+const router: Routes = [
+  { path: 'home', component: MyHomeComponent }
+];
 export class MyAboutComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Routes) { }
 
   ngOnInit() {
   }
 
 
 }
-// goToHome() {
-//   this.router.navigate(["/home"]);
-// }
+goToHome() {
+  this.router.navigate(["/home"]);
+}
