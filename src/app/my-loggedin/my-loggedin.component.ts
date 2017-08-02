8867fc21b19d01460532c69c3245939816ec7316
Arkadiusz <asmilewski@gmail.com>
2017-08-02 15:10:22 +0200

    logout button working( removing a session)

--- src/app/my-loggedin/my-loggedin.component.ts
+++ src/app/my-loggedin/my-loggedin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../services/session.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-loggedin',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyLoggedinComponent implements OnInit {
 
-  constructor() { }
+   user: Object = {
+    username: '',
+    password: ''
+ }
+
+ error = null;
+
+  constructor(private session: SessionService,
+              private router: Router)
+   { }
 
   ngOnInit() {
+  }
+
+  logout() {
+  this.router.navigate(['/logIn']);
+   console.log('logged out');
+
+ 
   }
 
 }
